41d8fe6c2af6457bfedccbc686e9f2306217066b
akrockzz123 <aniketkumar1601@gmail.com>
2021-09-25 02:03:27 +0530

    added max heap

---
+++ Heap.js
@@ -0,0 +1,98 @@
+class BinaryHeap {
+    constructor() {
+
+        this.heap = [];
+    }
+
+        size() {
+            return this.heap.length;
+        }
+
+        empty() {
+            return ( this.size() === 0 );
+        }
+
+        insert(value) {
+            this.heap.push(value)
+            this.bubbleUp();
+        }
+
+        bubbleUp() {
+            let index = this.size()-1;
+
+            while(index > 0)
+            {
+                let element = this.heap[index];
+                let parentIndex = Math.floor((index-1)/2);
+
+                let parent = this.heap[parentIndex]
+
+                if(parent[0] > element[0])
+                {
+                    break;
+                }
+                else
+                {
+                    this.heap[index] = parent
+                    this.heap[parentIndex] = element
+                    this.index = parentIndex;
+                }
+            }
+        }
+
+
+        extractMax() {
+            let mx = this.heap[0];
+
+            let lt = this.heap.pop()
+
+            if(!this.empty())
+            {
+                this.heap[0] = lt;
+                this.sinkdown(0);
+            }
+
+            return mx;
+        }
+
+        sinkdown(index)
+        {   
+            let left =2*index + 1
+
+            let right = 2*index + 2
+
+            let largest = index
+            let length = this.size();
+
+            if(left < length && this.heap[left][0] > this.heap[largest][0])
+            {
+                largest = left
+            }
+
+            if(right < length && this.heap[right][0] > this.heap[largest][0])
+            {
+                largest = right
+            }
+
+            if(largest != index)
+            {
+                let temp = this.heap[largest]
+                this.heap[largest] = this.heap[index]
+                this.heap[index] = temp
+                this.sinkdown(index)
+            }
+        }
+    }
+
+    let mxheap = new BinaryHeap();
+
+    mxheap.insert([4,5]);
+    mxheap.insert([1,3]);
+    mxheap.insert([1,4]);
+    mxheap.insert([6,1]);
+
+    while(!mxheap.empty())
+    {
+        let mx =  mxheap.extractMax()
+        console.log(mx)
+    }
